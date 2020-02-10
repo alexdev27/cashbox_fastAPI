dec5684afa5fe6a0438fcad73f06242ddc36152d
Aleksey Luzin <luzin.a@samberi.com>
2020-02-10 18:36:25 +1000

    added new schemas and commented unnecessary fields

--- app/cashbox/insert_remove/schemas.py
+++ app/cashbox/insert_remove/schemas.py
@@ -1,21 +1,35 @@
 from pydantic import Field
 from app.schemas import CashierData, DefaultSuccessResponse
+from marshmallow_mongoengine import ModelSchema, fields
+from .models import PayGateCashOperation
 
 
 class RequestCashIn(CashierData):
-    amount: float = Field(..., title='Количество вносимых денег (в рублях)', ge=0.5)
+    amount: float = Field(..., title='Количество вносимых денег (в рублях)', ge=0.1)
 
 
 class RequestCashOut(CashierData):
-    amount: float = Field(..., title='Количество вносимых денег (в рублях)', ge=0.5)
+    amount: float = Field(..., title='Количество вносимых денег (в рублях)', ge=0.1)
 
 
 class ResponseCashIn(DefaultSuccessResponse):
-    amount: float = Field(..., title='Сколько было внесено')
-    msg: str = Field(..., title='Сообщение о совершенной операции')
+    # amount: float = Field(..., title='Сколько было внесено')
+    msg: str = Field('Успешно', title='Сообщение о совершенной операции')
 
 
 class ResponseCashOut(DefaultSuccessResponse):
-    amount: float = Field(..., title='Сколько было изъято')
-    msg: str = Field(..., title='Сообщение о совершенной операции')
+    # amount: float = Field(..., title='Сколько было изъято')
+    msg: str = Field('Успешно', title='Сообщение о совершенной операции')
 
+
+class DBPaygateCashOperation(ModelSchema):
+    class Meta:
+        model = PayGateCashOperation
+
+
+class DBPaygateCashInSchema(DBPaygateCashOperation):
+    pass
+
+
+class DBPaygateCashOutSchema(DBPaygateCashOperation):
+    pass
