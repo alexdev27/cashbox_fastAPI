be9a90e2e68c2413a2f3d764b32dcbfdc0ac3774
Aleksey Luzin <luzin.a@samberi.com>
2020-02-10 16:20:29 +1000

    added map_to_field function

--- app/cashbox/insert_remove/models.py
+++ app/cashbox/insert_remove/models.py
@@ -1,6 +1,7 @@
 from mongoengine import Document, DateTimeField, FloatField, \
     BooleanField, IntField, StringField
 from datetime import datetime
+from dateutil import parser
 
 
 class CashOperation(Document):
@@ -11,3 +12,12 @@ class CashOperation(Document):
     cashier_name = StringField(required=True)
     cashier_id = StringField(required=True)
     document_type = IntField(required=True)
+
+    def map_to_fields(self, data):
+        self.creation_date = parser.parse(data['operation_time'])
+        self.amount = data['amount']
+        self.cashier_name = data['cashier_name']
+        self.cashier_id = data['cashier_id']
+        self.document_type = data['doc_type']
+        self.save().reload()
+        return self
