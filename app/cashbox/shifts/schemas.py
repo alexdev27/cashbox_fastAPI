8e3d79805d1faa4a9a95809ff54c3b05de30b11f
Aleksey Luzin <luzin.a@samberi.com>
2020-02-13 19:48:50 +1000

    new schemas with mapping to fields to load from

--- app/cashbox/shifts/schemas.py
+++ app/cashbox/shifts/schemas.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from marshmallow_mongoengine import ModelSchema, fields
-from .models import PayGateOpenShift, PayGateCloseShift
+from .models import OpenShift, CloseShift
 from app.schemas import CashierData, DefaultSuccessResponse
 from pydantic import Field
 
@@ -32,11 +32,59 @@ class ResponseCurrentShiftInfo(DefaultSuccessResponse):
     start_shift_money: float = Field(..., title='Денег в кассе на начало смены')
 
 
-class DBPaygateOpenShiftSchema(ModelSchema):
+class OpenShiftSchema(ModelSchema):
+    start_shift_money = fields.Float(load_from='cash_balance')
+    creation_date = fields.DateTime(load_from='datetime', required=True)
+
+    cashID = fields.Str(load_from='fn_number', required=True)
+    cashier = fields.Str(load_from='cashier_name', required=True)
+    cashierID = fields.Str(load_from='cashier_id', required=True)
+    shop = fields.Int(load_from='shop_number', required=True)
+    cashNumber = fields.Int(load_from='cash_number', required=True)
+    shiftNumber = fields.Int(load_from='shift_number', required=True)
+    cashBalanceOpen = fields.Float(load_from='cash_balance', required=True)
+    inn = fields.Str(load_from='inn', required=True)
+    totalSellOpen = fields.Float(load_from='progressive_total_sales', required=True)
+    totalReturnOpen = fields.Float(load_from='progressive_total_returns', required=True)
+    cashName = fields.Str(load_from='cash_name', required=True)
+    cashSerial = fields.Str(load_from='fn_number', required=True)
+    systemID = fields.Str(load_from='system_id', required=True)
+    proj = fields.Int(load_from='project_number', required=True)
+
     class Meta:
-        model = PayGateOpenShift
+        model = OpenShift
+
+    @staticmethod
+    def get_data_for_paygate(shift_object):
+        _fields = [
+            'cashID', 'cashier', 'cashierID', 'shop',
+            'cashNumber', 'shiftNumber', 'cashBalanceOpen',
+            'inn', 'totalSellOpen', 'totalReturnOpen',
+            'cashName', 'cashSerial', 'systemID', 'proj',
+        ]
+        return OpenShiftSchema(only=_fields).dump(shift_object).data
 
 
-class DBPaygateCloseShiftSchema(ModelSchema):
+class CloseShiftSchema(ModelSchema):
+    creation_date = fields.DateTime(load_from='datetime', required=True)
+    cashID = fields.Str(load_from='fn_number', required=True)
+    docNumber = fields.Int(load_from='doc_number', required=True)
+    cashBalanceClose = fields.Float(load_from='cash_balance', required=True)
+    totalSellClose = fields.Float(load_from='progressive_total_sales', required=True)
+    totalReturnClose = fields.Float(load_from='progressive_total_returns', required=True)
+    inAmount = fields.Float(load_from='sum_insert', required=True)
+    outAmount = fields.Float(load_from='sum_remove', required=True)
+    inCount = fields.Float(load_from='count_insert', required=True)
+    outCount = fields.Float(load_from='count_remove', required=True)
+    discountAmount = fields.Float(load_from='discount_sum_sales', required=True)
+    discountReturns = fields.Float(load_from='discount_sum_returns', required=True)
+    amount = fields.Float(load_from='sum_sales', required=True)
+    returns = fields.Float(load_from='sum_returns', required=True)
+
     class Meta:
-        model = PayGateCloseShift
+        model = CloseShift
+
+    @staticmethod
+    def get_data_for_paygate(shift_object):
+        _fields = ['cashier_name', 'cashier_id', 'creation_date']
+        return CloseShiftSchema(exclude=_fields).dump(shift_object).data
