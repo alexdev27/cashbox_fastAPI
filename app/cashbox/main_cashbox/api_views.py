8a7754db38c69dad91815bc07a8221f5456444c6
Aleksey Luzin <luzin.a@samberi.com>
2020-02-13 20:24:58 +1000

    added new route to get system identifier

--- app/cashbox/main_cashbox/api_views.py
+++ app/cashbox/main_cashbox/api_views.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 from .schemas import RequestRegisterCashboxCharacter, ResponseRegisterCashboxCharacter
-from .doc_kwargs import doc_register_character
-from .functions import register_cashbox_character
+from .doc_kwargs import doc_register_character, doc_get_sys_id
+from .functions import register_cashbox_character, get_sys_id
 
 
 router = APIRouter()
@@ -12,3 +12,8 @@ async def register_character(character: RequestRegisterCashboxCharacter):
     kwargs = {'valid_schema_data': character.dict()}
     data = await register_cashbox_character(**kwargs)
     return ResponseRegisterCashboxCharacter(**data)
+
+
+@router.get('/get_system_id', **doc_get_sys_id)
+def get_system_id():
+    return get_sys_id()
