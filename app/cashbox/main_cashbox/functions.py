9456d84b984cc912b94dd6d0129788d4be207d01
Aleksey Luzin <luzin.a@samberi.com>
2020-02-15 14:26:50 +1000

    comment print

--- app/cashbox/main_cashbox/functions.py
+++ app/cashbox/main_cashbox/functions.py
@@ -21,7 +21,7 @@ async def init_cashbox(*args, **kwargs):
 
     # # proj - номер системы, с которой происходит запрос
     obj = {'shop': shop_num, 'cashID': cash_id, 'systemID': sys_id, 'proj': 1}
-    print('TO paygate -> ', obj)
+    # print('TO paygate -> ', obj)
     paygate_content = await request_to_paygate(PaygateURLs.register_cash, 'post', obj)
 
     cash_num = paygate_content.get('cashNumber')
