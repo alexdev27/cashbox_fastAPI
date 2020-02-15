e9c1d3f8dc6af1e2986c8cac56f6fe9eb2aa3bd9
Aleksey Luzin <luzin.a@samberi.com>
2020-02-15 18:46:54 +1000

    refactor  insert/remove

--- app/cashbox/insert_remove/functions.py
+++ app/cashbox/insert_remove/functions.py
@@ -1,7 +1,6 @@
 from app.kkt_device.decorators import kkt_comport_activation, validate_kkt_state, \
     check_for_opened_shift_in_fiscal
-from .models import CashOperation
-from .schemas import DBPaygateCashInSchema, DBPaygateCashOutSchema
+from .schemas import CashOperationSchema
 from app.enums import DocumentTypes, PaygateURLs
 from app.kkt_device.models import KKTDevice
 from app.cashbox.main_cashbox.models import Cashbox
@@ -14,7 +13,6 @@ async def handle_insert(*args, **kwargs):
     await handle_money_transfer(
         *args, **kwargs,
         document_type=DocumentTypes.INSERT,
-        paygate_model=DBPaygateCashInSchema,
         paygate_url=PaygateURLs.insert_cash
     )
     return {}
@@ -27,7 +25,6 @@ async def handle_remove(*args, **kwargs):
     await handle_money_transfer(
         *args, **kwargs,
         document_type=DocumentTypes.REMOVE,
-        paygate_model=DBPaygateCashOutSchema,
         paygate_url=PaygateURLs.remove_cash
     )
     return {}
@@ -41,18 +38,20 @@ async def handle_money_transfer(*args, **kwargs):
     amount = req_data['amount']
     pennies = int(amount * 100)
     doc_type = kwargs['document_type']
-    paygate_model = kwargs['paygate_model']
     paygate_url = kwargs['paygate_url']
 
     kkt_result = KKTDevice.insert_remove_operation(cashier_name, pennies, doc_type)
 
     data_to_db = {
-        'cashier_id': cashier_id, 'cashier_name': cashier_name,
-        'amount': amount, 'document_type': doc_type}
-    operation = CashOperation().map_to_fields({**kkt_info, **kkt_result, **data_to_db})
+        'cashier_id': cashier_id,
+        'cashier_name': cashier_name,
+        'amount': amount,
+        'document_type': doc_type
+    }
+    operation, errs = CashOperationSchema().load({**kkt_info, **kkt_result, **data_to_db})
     cashbox.add_cash_operation_to_shift(operation)
     cashbox.update_shift_money_counter(doc_type, amount)
-    data_to_paygate = paygate_model().dump(operation.paygate_data).data
+    data_to_paygate = CashOperationSchema(only=['cashID', 'amount']).dump(operation).data
     data_to_paygate.update({'url': paygate_url})
     cashbox.save_paygate_data_for_send(data_to_paygate)
 
