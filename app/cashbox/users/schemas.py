2f38b2e5f8b0fa180394bc4a5b24f5b45b3ec3fe
alex <luzin.a@samberi.com>
2020-06-25 17:57:06 +1000

    some schemas

--- app/cashbox/users/schemas.py
+++ app/cashbox/users/schemas.py
@@ -1,15 +1,40 @@
+from typing import List
+
 from pydantic import Field, BaseModel
+from app.enums import UserRoles
 
 
-class RequestCreateUser(BaseModel):
-    pass
+class BaseFields:
+    fio: str = Field(default='', title='ФИО пользователя')
+    fio_required: str = Field(..., title='ФИО пользователя', min_length=3)
+    login: str = Field(default='', title='Идентификатор пользователя')
+    login_required: str = Field(..., title='Идентификатор пользователя', min_length=3)
+    password: str = Field(default='', title='Пароль пользователя (будет зашифрован)')
+    password_required: str = Field(..., title='Пароль пользователя (будет зашифрован)', min_length=4)
+    roles: List[UserRoles] = Field(default=[], title='Роли пользователя')
+    roles_required: List[UserRoles] = Field(..., title='Роли пользователя')
 
 
-class RequestGetUser(BaseModel):
-    pass
+class AllRequired(BaseModel):
+    fio: str = BaseFields.fio_required
+    login: str = BaseFields.login_required
+    roles: List[UserRoles] = BaseFields.roles_required
 
 
-class RequestGetUsers(BaseModel):
+class EngineerUserRequired(BaseModel):
+    engineer_login: str = Field(..., title='Логин инженера')
+    engineer_password: str = Field(..., title='Пароль инженера')
+
+
+class RequestCreateUser(EngineerUserRequired, AllRequired):
+    password: str = BaseFields.password_required
+
+
+class RequestGetUser(EngineerUserRequired):
+    id: str
+
+
+class RequestGetUsers(EngineerUserRequired):
     pass
 
 
