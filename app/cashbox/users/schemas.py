d2121df526c5cacbb60be0d553e71cd0d5ba6448
alex <luzin.a@samberi.com>
2020-06-25 15:23:12 +1000

    some boilerplate

---
+++ app/cashbox/users/schemas.py
@@ -0,0 +1,17 @@
+from pydantic import Field, BaseModel
+
+
+class RequestCreateUser(BaseModel):
+    pass
+
+
+class RequestGetUser(BaseModel):
+    pass
+
+
+class RequestGetUsers(BaseModel):
+    pass
+
+
+# class
+
