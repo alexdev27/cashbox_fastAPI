6273747e9e2cf473e9e24d95283283828b5d5ce2
alex <luzin.a@samberi.com>
2020-07-01 08:07:14 +1000

    schemes for user operations

--- app/cashbox/users/schemas.py
+++ app/cashbox/users/schemas.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 from pydantic import Field, BaseModel
 from app.enums import UserRoles
@@ -8,14 +8,14 @@ from app.custom_types import ObjectIdStr
 class BaseFields:
     id: ObjectIdStr = Field(default='', title='ID пользователя в базе')
     id_required: ObjectIdStr = Field(..., title='ID пользователя в базе')
-    fio: str = Field(default='', title='ФИО пользователя')
+    fio: str = Field(default='', title='ФИО пользователя', min_length=3)
     fio_required: str = Field(..., title='ФИО пользователя', min_length=3)
-    login: str = Field(default='', title='Идентификатор пользователя')
+    login: str = Field(default='', title='Идентификатор пользователя', min_length=3)
     login_required: str = Field(..., title='Идентификатор пользователя', min_length=3)
-    password: str = Field(default='', title='Пароль пользователя')
+    password: str = Field(default='', title='Пароль пользователя', min_length=4)
     password_required: str = Field(..., title='Пароль пользователя', min_length=4)
-    roles: List[UserRoles] = Field(default=[], title='Роли пользователя')
-    roles_required: List[UserRoles] = Field(..., title='Роли пользователя')
+    roles: List[UserRoles] = Field(default=[], title='Роли пользователя', min_items=1)
+    roles_required: List[UserRoles] = Field(..., title='Роли пользователя', min_items=1)
 
 
 class AllRequired(BaseModel):
@@ -24,24 +24,44 @@ class AllRequired(BaseModel):
     roles: List[UserRoles] = BaseFields.roles_required
 
 
+class AllOptional(BaseModel):
+    fio: Optional[str] = BaseFields.fio
+    login: Optional[str] = BaseFields.login
+    roles: Optional[List[UserRoles]] = BaseFields.roles
+
+
 class EngineerUserCredentials(BaseModel):
     engineer_login: str = Field(..., title='Логин инженера')
     engineer_password: str = Field(..., title='Пароль инженера')
 
 
-class RequestCreateUser(EngineerUserCredentials, AllRequired):
+class RequestCreateUser(AllRequired):
     password: str = BaseFields.password_required
 
 
-class RequestGetUser(EngineerUserCredentials):
-    id: ObjectIdStr = BaseFields.id_required
-    fio: str
-    roles: List[UserRoles]
-    login: str
+class ResponseCreateUser(AllRequired):
+    class Config:
+        orm_mode = True
 
 
-class RequestGetUsers(EngineerUserCredentials):
-    pass
+class ResponseGetUser(AllRequired):
+    class Config:
+        orm_mode = True
+
+# class UserForView(BaseModel):
+#     id: ObjectIdStr = BaseFields.id_required
+#     fio: str
+#     roles: List[UserRoles]
+#     login: str
+
+
+# class RequestGetUsers(BaseModel):
+#     users: List[]
+
+
+class RequestModifyUser(AllOptional):
+    password: Optional[str] = BaseFields.password
+
 
 
 
