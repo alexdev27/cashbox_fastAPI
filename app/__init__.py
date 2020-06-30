a1147958efe4647256b5747442159faa9522626c
alex <luzin.a@samberi.com>
2020-07-01 09:57:12 +1000

    include users router

--- app/__init__.py
+++ app/__init__.py
@@ -39,9 +39,9 @@ from mongoengine import connect, disconnect
 import config
 from .logging import get_logger
 
-# from app.kkt_device.models import KKTDevice as Pirit2f
-# from app.kkt_spark115f.models import Spark115f
-# KKTDevice = Spark115f
+
+# oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
+
 
 # mongo connection
 connect(
@@ -98,7 +98,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
+#
 app.include_router(
     router=insert_remove_router,
     prefix=url_with_prefix(),
@@ -118,16 +118,110 @@ app.include_router(
     tags=['Заказы (совершение оплаты/отмены)'])
 
 
-# app.include_router(
-#     router=users_router,
-#     prefix=url_with_prefix(),
-#     tags=['Операции с пользователями']
-# )
+app.include_router(
+    router=users_router,
+    prefix=url_with_prefix(),
+    tags=['Операции с пользователями']
+)
 
 app.include_router(
     router=cashbox_router,
     prefix=url_with_prefix(),
     tags=['Другие операции над кассой'])
+
+# "hashed_password": "$2b$10$5AEINtothQlazoHtaYqjK.ztGYqsoDKhNNbGB1fyzFSoZXWpjOgP2",
+
+# from typing import Optional
+#
+# from fastapi import Depends, FastAPI, HTTPException
+# from starlette.status import HTTP_401_UNAUTHORIZED
+# from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+# from pydantic import BaseModel
+#
+# fake_users_db = {
+#     "johndoe": {
+#         "username": "johndoe",
+#         "full_name": "John Doe",
+#         "email": "johndoe@example.com",
+#         "hashed_password": "fakehashedsecret",
+#         "disabled": False,
+#     },
+#     "alice": {
+#         "username": "alice",
+#         "full_name": "Alice Wonderson",
+#         "email": "alice@example.com",
+#         "hashed_password": "fakehashedsecret2",
+#         "disabled": True,
+#     },
+# }
+#
+# app = FastAPI()
+#
+#
+# def fake_hash_password(password: str):
+#     return "fakehashed" + password
+#
+#
+# oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
+#
+#
+# class User(BaseModel):
+#     username: str
+#     email: Optional[str] = None
+#     full_name: Optional[str] = None
+#     disabled: Optional[bool] = None
+#
+#
+# class UserInDB(User):
+#     hashed_password: str
+#
+#
+# def get_user(db, username: str):
+#     if username in db:
+#         user_dict = db[username]
+#         return UserInDB(**user_dict)
+#
+#
+# def fake_decode_token(token):
+#     # This doesn't provide any security at all
+#     # Check the next version
+#     user = get_user(fake_users_db, token)
+#     return user
+#
+#
+# async def get_current_user(token: str = Depends(oauth2_scheme)):
+#     user = fake_decode_token(token)
+#     if not user:
+#         raise HTTPException(
+#             status_code=HTTP_401_UNAUTHORIZED,
+#             detail="Invalid authentication credentials",
+#             headers={"WWW-Authenticate": "Bearer"},
+#         )
+#     return user
+#
+#
+# async def get_current_active_user(current_user: User = Depends(get_current_user)):
+#     if current_user.disabled:
+#         raise HTTPException(status_code=400, detail="Inactive user")
+#     return current_user
+#
+#
+# @app.post("/token")
+# async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+#     user_dict = fake_users_db.get(form_data.username)
+#     if not user_dict:
+#         raise HTTPException(status_code=400, detail="Incorrect username or password")
+#     user = UserInDB(**user_dict)
+#     hashed_password = fake_hash_password(form_data.password)
+#     if not hashed_password == user.hashed_password:
+#         raise HTTPException(status_code=400, detail="Incorrect username or password")
+#
+#     return {"access_token": user.username, "token_type": "bearer"}
+#
+#
+# @app.get("/users/me")
+# async def read_users_me(current_user: User = Depends(get_current_active_user)):
+#     return current_user
 
 
 def custom_openapi():
@@ -160,6 +254,7 @@ async def async_startup():
     try:
         await init_cashbox()
         KKTDevice.startup()
+        #pass
     except CashboxException as c_exc:
         msg = f'{c_exc.__class__.__name__}: {c_exc.data["errors"]}'
         startup_logger.error(msg)
